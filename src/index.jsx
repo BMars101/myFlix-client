2c0ad4156254c0b19135206cb010e523ad54f06d
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-05-24 09:10:53 -0400

    added redux store to file.

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import './index.scss';
+import MainView from './components/main-view/main-view';
 
-
-import { MainView } from './components/main-view/main-view';
+const store = createStore(moviesApp, devToolsEnhancer());
 
 
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <MainView />
+      <Provider store={store}> 
+        <MainView />
+      </Provider>
+      
     );
   }
 }
